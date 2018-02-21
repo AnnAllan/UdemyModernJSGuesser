dd56c1a08773199f4af5e9ccc2539754ae543813
Ann Allan <ann.cristina.allan@gmail.com>
2018-02-21 18:16:03 -0500

    Add part 4

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ GAME FUNCTION
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI Elements
@@ -24,6 +24,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and maxNum
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// play again Event Listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+});
 
 // Listen for guess`
 guessBtn.addEventListener('click', function() {
@@ -67,6 +74,14 @@ function gameOver(won, msg){
     // Set message
     setMessage(msg);
 
+    // Play Again?
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max){
+    return (Math.floor(Math.random() * (max - min + 1) )+ min);
 }
 
 // Set setMessage
